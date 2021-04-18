cef98d1de5c3900973dbd6ec381050c0fe522406
Theodora2000 <xsimont@stuba.sk>
2021-04-18 14:40:39 +0200

    generic index

---
+++ src/main/java/sk/stuba/uim/fei/oop/Generic.java
@@ -0,0 +1,36 @@
+package sk.stuba.uim.fei.oop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Generic<KEY_TYPE, VALUE_TYPE> {
+
+    private Map<KEY_TYPE, List<VALUE_TYPE>> index;
+
+    public Generic(){
+        this.index= new HashMap<>();
+
+    }
+
+    public List<VALUE_TYPE>put (KEY_TYPE key, VALUE_TYPE value){
+        var currentValue=this.index.get(key);
+        List<VALUE_TYPE> oldValue=null;
+        if(currentValue!= null){
+            oldValue = new ArrayList<>(currentValue);
+        }
+       if (this.index.containsKey(key)) {
+            this.index.get(key).add(value);
+        }else{
+            List<VALUE_TYPE> newList = new ArrayList<>();
+            newList.add(value);
+            this.index.put(key, newList);
+        }
+
+        return oldValue;
+
+    }
+
+
+}
